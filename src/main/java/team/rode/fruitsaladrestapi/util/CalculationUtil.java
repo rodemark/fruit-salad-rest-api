cacbd2db4dae5d7af2130282e54899ae8a8dff3d
rodemark <rode.mark01@gmail.com>
2025-03-27 11:28:10 +0300

    fix: correct private constructor in CalculationUtil

--- src/main/java/team/rode/fruitsaladrestapi/util/CalculationUtil.java
+++ src/main/java/team/rode/fruitsaladrestapi/util/CalculationUtil.java
@@ -7,8 +7,7 @@ public final class CalculationUtil {
     public static final double BASE_NUTRITION_UNIT = 100.0;
     public static final int DECIMAL_SCALE = 1;
 
-    private void CalculationUtils() {
-    }
+    private CalculationUtil() {}
 
     public static double round(double value) {
         return new BigDecimal(value)
