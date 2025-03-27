cdc3c94afef6fc512812ad5cc34dbc8ce4ec6533
rodemark <rode.mark01@gmail.com>
2025-03-27 20:15:36 +0300

    fix: correct NotNull import

--- src/test/java/team/rode/fruitsaladrestapi/services/NutritionCalculatorServiceTest.java
+++ src/test/java/team/rode/fruitsaladrestapi/services/NutritionCalculatorServiceTest.java
@@ -1,6 +1,6 @@
 package team.rode.fruitsaladrestapi.services;
 
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
